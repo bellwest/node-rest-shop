e0ebfcbb6393d72e1b06bcf299494fa43d629727
bellwest <bellwest93@gmail.com>
2021-07-11 00:37:44 +0900

    part 9 - how to populate queries

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,6 +8,7 @@ const Product = require('../models/product');
 router.get('/', (req, res, next) => {
     Order.find()
       .select('product quantity _id')
+      .populate('product', 'name')
       .exec()
       .then(docs => {
           res.status(200).json({
@@ -80,6 +81,7 @@ router.post('/:orderId', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
+      .populate('product')
       .exec()
       .then(order => {
           if (!order) {
