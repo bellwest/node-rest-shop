e6688aa2d50ee780605a166e3d926dd951781fcd
bellwest <bellwest93@gmail.com>
2021-07-11 17:14:12 +0900

    part 10 - how to upload an image

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb+srv://bellwest:' + process.env.MONGO_ATLAS_PW + '@clus
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
